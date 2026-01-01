987bbb89136cc3a9637e97e13751d409cb400e18
Armand Amores <aj_amores@hotmail.com>
2026-01-01 15:51:11 -0500

    - added remaining sections
    - about me, experience, projects, contact page added
    -footer added
    - need to fill out links
    - need to add animations
    - need to add mobile styling

--- script.js
+++ script.js
@@ -6,15 +6,22 @@ experienceButtons.forEach((button, index) => {
     button.addEventListener('click', ()=> {
         
         //make each button not active
-        experienceButtons.forEach(button=>{button.classList.remove('active')});
+        experienceButtons.forEach(button=>{
+            button.classList.remove('active');
+            button.style.backgroundColor = 'white';
+            button.style.color = 'black';
+        });
+
         
         //except for the one pressed, cause remember we add event listener to each button 
         button.classList.add('active');
+
 
         const category = button.innerHTML
         console.log(category)
 
         if(category === 'Experience'){
+            
             experienceItems.forEach(item=>{
                 item.style.display = 'flex';
             });
@@ -23,6 +30,7 @@ experienceButtons.forEach((button, index) => {
                 item.style.display = 'none';
             })
         } else if(category ==='Education'){
+
             
             experienceItems.forEach(item=>{
                 item.style.display = 'none';
@@ -31,11 +39,62 @@ experienceButtons.forEach((button, index) => {
             educationItems.forEach(item=>{
                 item.style.display='flex';
             });
-            
-
         }
+
+            button.style.backgroundColor = 'black';
+            button.style.color = 'white';
+
+
     })
 });
+
+
+const leftBtn = document.querySelector('.left');
+const rightBtn = document.querySelector('.right');
+const projectInfo = document.querySelectorAll('.project-info');
+let index = 0;
+
+leftBtn.addEventListener('click' , () =>{
+    console.log('left button pressed');
+    index--;
+
+    if(index < 0){
+        index = projectInfo.length - 1
+    }
+
+    console.log("Index: " + index);
+
+    projectInfo.forEach(project =>{
+        project.classList.remove('active')
+    })
+
+    projectInfo[index].classList.add('active');
+
+});
+
+rightBtn.addEventListener('click' , () =>{
+    console.log('right button pressed');
+    index++;
+
+    if(index >= projectInfo.length){
+        index = 0;
+    }
+
+    console.log("Index: " + index);
+
+    projectInfo.forEach(project =>{
+        project.classList.remove('active')
+    })
+
+    projectInfo[index].classList.add('active');
+
+});
+
+
+
+
+
+
 
 
 
